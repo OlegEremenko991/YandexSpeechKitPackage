63d696162dcf9a74254b326994d4c9df02c8731f
Oleg991 <51740599+OlegEremenko991@users.noreply.github.com>
2022-01-13 19:07:28 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# YandexSpeechKitPackage
+
+A description of this package.
---
+++ Sources/YandexSpeechKitPackage/YandexSpeechKitPackage.swift
@@ -0,0 +1,6 @@
+public struct YandexSpeechKitPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
